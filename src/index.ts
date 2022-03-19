8ce4d00a5334fee2a74217679de4515e21732666
Jonggu Lee <jg.jake.lee@gmail.com>
2022-03-19 20:02:00 +0900

    Adding calculate hash in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { timeStamp } from "console";
+import * as CryptoJS from "crypto-js";
 
 class Block {
     public index: number;
@@ -6,6 +6,14 @@ class Block {
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockHash = (
+        index: number,
+        previousHash: string,
+        timestamp: number,
+        data: string
+    ):string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(
         index: number,
         hash: string,
@@ -23,7 +31,13 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "12095891285", "", "Hello", 123456)
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain;
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000)
+
+
 
 console.log(blockchain);
 
